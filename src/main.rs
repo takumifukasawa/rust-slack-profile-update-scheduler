e46db9149d06817642a296714280178062482b9f
takumifukasawa <takumifukasawa@gmail.com>
2021-11-25 22:50:06 +0900

    捕色で生成

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate rayon;
 
 use image::DynamicImage;
 use image::GenericImageView;
-use image::{ImageBuffer, Rgb, RgbImage, Rgba};
+use image::{Rgb, RgbImage};
 use rand::Rng;
 use rayon::prelude::*;
 
@@ -14,44 +14,32 @@ fn generate_image(src_img: DynamicImage, rgb: Vec<u8>) -> RgbImage {
 
     let min = rgb.iter().fold(0, |m, v| *v.min(&m));
     let max = rgb.iter().fold(0, |m, v| *v.max(&m));
-    let base_color = max + min;
-
-    // let pixel: Rgba<u8> = img.get_pixel(x, y);
-    // let r = pixel[0];
-    // let g = pixel[1];
-    // let b = pixel[2];
-    // let rgb = vec![r, g, b];
-    // let min = rgb.iter().fold(0, |m, v| *v.min(&m));
-    // let max = rgb.iter().fold(0, |m, v| *v.max(&m));
+    let complementary_base_color = max + min;
 
     img.enumerate_pixels_mut()
         .collect::<Vec<(u32, u32, &mut Rgb<u8>)>>()
         .par_iter_mut()
         .for_each(|(x, y, pixel)| {
-            pixel[0] = rgb[0];
-            pixel[1] = rgb[1];
-            pixel[2] = rgb[2];
-            // pixel[0] = base_color - r;
-            // pixel[1] = base_color - g;
-            // pixel[2] = base_color - b;
+            let current_profile_color = src_img.get_pixel(*x, *y);
+            if current_profile_color[0] > 127 {
+                pixel[0] = rgb[0];
+                pixel[1] = rgb[1];
+                pixel[2] = rgb[2];
+            } else {
+                pixel[0] = complementary_base_color - rgb[0];
+                pixel[1] = complementary_base_color - rgb[1];
+                pixel[2] = complementary_base_color - rgb[2];
+            }
         });
 
     return img;
 }
 
 fn main() {
-    // let mut img = image::open("images/profile-icon.png").unwrap();
-    // img.invert();
-    // img.save("invert.png").unwrap();
-    // println!("dimensions {:?}", img.dimensions());
-
     let src_path = "images/profile-icon.png";
+    let output_path = "output.png";
 
     let img: DynamicImage = image::open(src_path).unwrap();
-
-    // let (width, height) = img.dimensions();
-
-    // let mut new_image: image::RgbImage = image::ImageBuffer::new(width, height);
 
     let mut rng = rand::thread_rng();
 
@@ -63,30 +51,5 @@ fn main() {
 
     let new_image = generate_image(img, rgb);
 
-    new_image.save("output.png").unwrap();
-
-    // for y in 0..height {
-    //     for x in 0..width {
-    //         let pixel: Rgba<u8> = img.get_pixel(x, y);
-    //         let r = pixel[0];
-    //         let g = pixel[1];
-    //         let b = pixel[2];
-    //         let rgb = vec![r, g, b];
-    //         let min = rgb.iter().fold(0, |m, v| *v.min(&m));
-    //         let max = rgb.iter().fold(0, |m, v| *v.max(&m));
-
-    //         let new_r =
-    //         // let max = &rgbIterator.fold(0, |m, &v| v.max(m));
-
-    //         // for debug
-    //         // println!("{:?}, {:?}", min, max);
-
-    //         // let base =
-    //         // for debug
-    //         // println!("{} {} {}", r, g, b);
-
-    //         break;
-    //     }
-    //         break;
-    // }
+    new_image.save(output_path).unwrap();
 }
