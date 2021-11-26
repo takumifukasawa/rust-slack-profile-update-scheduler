bd12af6f1a2e3e44028f4a727bc74d7e2c59cdd0
takumifukasawa <takumifukasawa@gmail.com>
2021-11-26 20:26:14 +0900

    post image

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 extern crate image;
 extern crate rayon;
 
+use std::env;
+use std::process::Command;
 use image::DynamicImage;
 use image::GenericImageView;
 use image::{Rgb, RgbImage};
 use rand::Rng;
 use rayon::prelude::*;
+use std::fs;
+
+const POST_URL: &str = "https://slack.com/api/users.setPhoto";
+const IMAGE_SRC: &str = "images/profile-icon.png";
+const OUTPUT_FILE: &str = "output.png";
+const TOKEN_SRC: &str = "token.txt";
 
 fn generate_image(src_img: DynamicImage, rgb: Vec<u8>) -> RgbImage {
     let (width, height) = src_img.dimensions();
@@ -35,9 +43,17 @@ fn generate_image(src_img: DynamicImage, rgb: Vec<u8>) -> RgbImage {
     return img;
 }
 
+fn build_absolute_path(src: &str) -> String {
+    let current_dir = env::current_dir().unwrap();
+    let path = String::from(current_dir.to_str().unwrap()) + "/" + src;
+    return path;
+}
+
+
 fn main() {
-    let src_path = "images/profile-icon.png";
-    let output_path = "output.png";
+    let src_path = build_absolute_path(IMAGE_SRC);
+    let output_path = build_absolute_path(OUTPUT_FILE);
+    let token_path = build_absolute_path(TOKEN_SRC);
 
     let img: DynamicImage = image::open(src_path).unwrap();
 
@@ -51,5 +67,34 @@ fn main() {
 
     let new_image = generate_image(img, rgb);
 
-    new_image.save(output_path).unwrap();
+    new_image.save(&output_path).unwrap();
+
+    // build token args
+    let token = "token=".to_string();
+    let raw_token = fs::read_to_string(token_path)
+        .expect("error when read token txt");
+    let token_arg = token + &raw_token;
+
+    let image_arg = "image=@".to_string() + &output_path;
+
+    let curl_command = Command::new("curl")
+        .arg("-X")
+        .arg("POST")
+        .arg(POST_URL)
+        .arg("-F")
+        .arg(token_arg)
+        .arg("-F")
+        .arg(image_arg)
+        .arg("-F")
+        .arg("pretty=1")
+        .output()
+        .expect("failed to execute.");
+
+    let command_stdout = curl_command.stdout;
+    let command_stderr = curl_command.stderr;
+
+    println!("{}", std::str::from_utf8(&command_stdout).unwrap());
+    println!();
+    println!("{}", std::str::from_utf8(&command_stderr).unwrap());
+    println!();
 }
